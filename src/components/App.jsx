1fb95fa8c40070d9c91224e7ffad024fa47f8f1e
danmaraba <danielkmaraba@gmail.com>
2024-07-29 20:27:03 +0300

    import APIProvider,Map and Marker components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React,{ useState } from 'react'
+import{APIProvider,Map,Marker} from '@vis.gl/react-google-maps'
 import '../App.css'
 function App() {
   const [count, setCount] = useState(0)
