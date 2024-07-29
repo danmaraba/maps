935a7c9fdded1ae367bc35e4870593253927ba39
danmaraba <danielkmaraba@gmail.com>
2024-07-29 21:15:11 +0300

    add style to map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import React,{ useState } from 'react'
-import{APIProvider,Map,Marker} from '@vis.gl/react-google-maps'
+import React from 'react'
+import{APIProvider,Map,AdvancedMarker} from '@vis.gl/react-google-maps'
 import '../App.css'
 function App() {
- const position={lat:0.05182700,long:35.71991000}
+ const position={lat:0.05182700,lng:35.71991000}
   return (
       <APIProvider apiKey={'AIzaSyCctTv1yocCEBTs6a8XnHcg-nAp_S1ahRI'}>
-        <Map center={position} zoom={10}>
-
+        <Map center={position} zoom={10} mapId={'ccaaba54a18c4cdd'} style={{width: '100%', height: '100vh'}}>
+          <AdvancedMarker position={position} />
         </Map>
       </APIProvider>
 
