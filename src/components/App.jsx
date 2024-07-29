d9adc87390ff4991f674c45199be583f8490b317
danmaraba <danielkmaraba@gmail.com>
2024-07-29 20:50:08 +0300

    add center and zoom properties

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,9 @@ function App() {
  const position={lat:0.05182700,long:35.71991000}
   return (
       <APIProvider apiKey={'AIzaSyCctTv1yocCEBTs6a8XnHcg-nAp_S1ahRI'}>
+        <Map center={position} zoom={10}>
 
+        </Map>
       </APIProvider>
 
   )
