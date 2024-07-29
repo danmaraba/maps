60a4c97093f9b1b3182244a1f27c925452fda2a0
danmaraba <danielkmaraba@gmail.com>
2024-07-29 20:19:03 +0300

    install vis.gl packages

---
+++ src/components/Use.jsx
@@ -0,0 +1,3 @@
+// How to use the vis.gl react google.maps library
+// It comes up with a collection of components to create maps, markers and info windows
+// To use it install it by running the code npm install  @vis.gl/react-google-maps
